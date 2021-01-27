0e00cbf950c14bcf7df5399b9b0f2df1dc5c4b9a
Elena <elentrade@rambler.ru>
2021-01-27 23:38:14 +0400

    lesson 5 homework OOP

---
+++ src/employee.java
@@ -0,0 +1,31 @@
+public class employee {
+    String Name;
+    String Position;
+    String email;
+    String Phone;
+    int Salary;
+    int Age;
+    employee(String Name, String Position,String email, String Phone,int Salary, int Age ){
+        this.Name=Name;
+        this.Position=Position;
+        this.email=email;
+        this.Phone=Phone;
+        this.Salary=Salary;
+        this.Age=Age;
+    }
+public void PrintPersonalData(){
+        System.out.println("Employee`s name:"+this.Name);
+        System.out.println("Position:"+this.Position);
+        System.out.println("Contacts:"+this.email+ ", tel: "+this.Phone);
+        System.out.println("Salary:"+this.Salary);
+        System.out.println("Age:"+this.Age);
+}
+    public boolean AgeFilter(){
+        if(this.Age>40){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
---
+++ src/lesson5.java
@@ -0,0 +1,21 @@
+public class lesson5 {
+    public static void main (String[]args){
+       employee Petrov = new employee("Petrov Serge","Division director","petrov@gmail.ru","+79376803322",200000,55);
+       Petrov.PrintPersonalData();
+
+        employee [] arr1 = new employee[5];
+        arr1[0]=  new employee("Ivanov Alex", "Sails manager", "ivanov@mail.ru","890090070",120000,30);
+        arr1[1]=  new employee("Voronin Alexander", "HR manager", "voron@mail.ru","890090010",80000,32);
+        arr1[2]= new employee("Stepanov Peter", "Ingeneer", "step@mail.ru","890090020",130000,46);
+        arr1[3]= new employee("Gusev Alex", "Chief accounter", "gusev@mail.ru","890090060",170000,42);
+        arr1[4]= new employee("Petrova Anna", "Business analist", "petrova@mail.ru","890090090",100000,33);
+
+
+        for (int i=0;i < arr1.length;i++) {
+          if(arr1[i].AgeFilter()) {
+            arr1[i].PrintPersonalData();
+          }
+        }
+    }
+
+}
